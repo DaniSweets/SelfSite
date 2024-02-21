747943a42c3b9061825362ff1304b60cc5d4e67a
Dani <danisweetwood@gmail.com>
2024-02-21 14:37:59 -0600

    added functionality to main and app

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Projects from './components/Projects';
+
+function App() {
+  return (
+    <div className="projects">
+      <Projects />
+    </div>
+  );
+}
+
+export default App;
