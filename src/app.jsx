275ee5a425ef2bbeca1ee76c09a454eef7786508
Dani <danisweetwood@gmail.com>
2024-02-27 13:53:33 -0600

    path fixes

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,20 @@
 import './App.css';
+import Contact from './components/Contact';
 import Projects from './components/Projects';
+import Resume from './components/Resume';
 
 function App() {
   return (
-    <div className="projects">
-      <Projects />
+    <div>
+      <div className='contact'>
+        <Contact />
+      </div>
+      <div className="projects">
+        <Projects />
+      </div>
+      <div className='resume'>
+        <Resume />
+      </div>
     </div>
   );
 }
