3bb465adff7fd6aeed8186aced8d56ecca2bcfa8
Dani <danisweetwood@gmail.com>
2024-02-27 20:46:33 -0600

    changed download and pdf path

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,7 +1,7 @@
 export default function Resume(){
     return (
 <div>
-<a href="../public/Danielle-Sweetwood.pdf"><img className="icon" id="download"
-      src="../public/downloads.png"></img></a>
+<a href="/Danielle-Sweetwood.pdf"><img className="icon" id="download"
+      src="/downloads.png"></img></a>
 </div>
 )};
