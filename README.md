fd2176dfe66160a476c67e85a876549955179fdd
Dani <danisweetwood@gmail.com>
2024-02-27 21:42:30 -0600

    added description and link to deployed page to the readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ReactPortfolio
+
+This application shows my previous projects as well as linking to GitHub, LinkedIn, and a PDF of my resume.
+
+The deployed application is here: https://danisweetwoodportfolio.netlify.app
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,6 +3,5 @@ export default function Resume(){
 <div>
     <h3>If you'd like to download my resume, just click here! <a href="/Danielle-Sweetwood.pdf"><img className="icon" id="download"
       src="/downloads.png"></img></a></h3>
-
 </div>
 )};
