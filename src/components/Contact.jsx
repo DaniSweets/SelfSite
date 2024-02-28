cd946f4cdf7fd9707ec2116459ef743b19235b11
Dani <danisweetwood@gmail.com>
2024-02-27 20:50:13 -0600

    moved icons for contact and fixed paths

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,8 @@
 export default function Contact() {
     return (
         <div>
-            <a href="https://github.com/DaniSweets"><img className="icon" id="github" src="./src/assets/logo.png"></img></a>
-            <a href="https://www.linkedin.com/in/dani-sweetwood/"><img className="icon" id="linkedIn" src="./src/assets/linkedin.png"></img></a>
+            <a href="https://github.com/DaniSweets"><img className="icon" id="github" src="/logo.png"></img></a>
+            <a href="https://www.linkedin.com/in/dani-sweetwood/"><img className="icon" id="linkedIn" src="/linkedin.png"></img></a>
         </div>
     )
 };
