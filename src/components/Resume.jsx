39471dbba34480e891169620e83cc7127313b90d
Dani <danisweetwood@gmail.com>
2024-02-27 20:41:06 -0600

    changed pathing for resume img and pdf

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,7 +1,7 @@
 export default function Resume(){
     return (
 <div>
-<a href="./src/assets/Danielle-Sweetwood.pdf"><img className="icon" id="download"
-      src="./src/assets/downloads.png"></img></a>
+<a href="../assets/Danielle-Sweetwood.pdf"><img className="icon" id="download"
+      src="../assets/downloads.png"></img></a>
 </div>
 )};
