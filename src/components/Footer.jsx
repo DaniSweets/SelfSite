ca8b8604c38836af656538a8772452176f07e6ad
Dani <danisweetwood@gmail.com>
2024-04-03 03:43:21 -0500

    removed redundant Resume component, cleaned up code for footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,15 @@
-import Resume from './Resume'
-
 export default function Footer() {
     return (
         <footer className="footer is-flex is-justify-content-space-around">
-                    <a href="https://github.com/DaniSweets" className='title m-3'>
-                        <ion-icon name="logo-github"></ion-icon>
-                    </a>
-                    <a href="https://www.linkedin.com/in/dani-sweetwood/" className='title m-3'>
-                        <ion-icon name="logo-linkedin"></ion-icon>
-                    </a>
-
-                    <a href='/Dani Sweetwood.pdf' className='title m-3'><ion-icon name="document-attach-outline"></ion-icon></a>
+            <a href="https://github.com/DaniSweets" className='title m-3'>
+                <ion-icon name="logo-github"></ion-icon>
+            </a>
+            <a href="https://www.linkedin.com/in/dani-sweetwood/" className='title m-3'>
+                <ion-icon name="logo-linkedin"></ion-icon>
+            </a>
+            <a href='/Dani Sweetwood.pdf' className='title m-3'>
+                <ion-icon name="document-attach-outline"></ion-icon>
+            </a>
         </footer>
     )
 }
