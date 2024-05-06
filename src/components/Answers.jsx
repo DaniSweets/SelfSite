3a9f1c2b2c5b1163ce33a4856f215ba5ab0b7d68
Dani <danisweetwood@gmail.com>
2024-05-06 00:40:06 -0500

    added questions

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,7 +1,74 @@
 export default function Answers() {
+
+    let questionsWithAnswers = [
+        {
+            question: "Are you currently employed?",
+            answer: ""
+        },
+        {
+            question: "What programming languages are you familiar with?",
+            answer: ""
+        },
+        {
+            question: "How do you structure a web page using HTML5?",
+            answer: ""
+        },
+        {
+            question: "How do you handle errors in JavaScript?",
+            answer: ""
+        },
+        {
+            question: "How do you optimize website performance?",
+            answer: ""
+        },
+        {
+            question: "Can you describe a challenging problem you faced in a previous project and howyousolvedit?",
+            answer: ""
+        },
+        {
+            question: "Explain the concept of responsive design.",
+            answer: ""
+        },
+        {
+            question: "How do you organize your workflow to complete web development projects?",
+            answer: ""
+        },
+        {
+            question: "How do you stay updated on current technology trends, news or products?",
+            answer: ""
+        },
+        {
+            question: "What steps have you taken in the past six months to develop your skills?",
+            answer: ""
+        },
+        {
+            question: "Would you be willing to learn new coding languages to complete projects?",
+            answer: ""
+        },
+        {
+            question: "What is your favorite framework to work in?",
+            answer: ""
+        },
+        {
+            question: "Describe your daily routine as a junior web developer.",
+            answer: ""
+        },
+        {
+            question: "What makes you stand out from other junior web developer candidates for this position?",
+            answer: ""
+        }
+    ]
+
     return (
         <section>
-            <p className="title">ANSWERS</p>
+            <div className="card">
+                <header className="card-header">
+                    <p className="card-header-title"></p>
+                </header>
+                <article className="card-content">
+                    <p></p>
+                </article>
+            </div>
         </section>
     )
 }
