34d07508e74f3d5e6e6962f6ba38169995a7ad04
Dani <danisweetwood@gmail.com>
2024-07-02 16:26:35 -0500

    updated Resume

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -1,11 +1,13 @@
-export default function Languages() {
-    return (
-        <div className="content has-text-centered">
-            <span className="title"><ion-icon name="logo-javascript"></ion-icon></span>
-            <span className="title"><ion-icon name="logo-nodejs"></ion-icon></span>
-            <span className="title"><ion-icon name="logo-react"></ion-icon></span>
-            <span className="title"><ion-icon name="logo-html5"></ion-icon></span>
-            <span className="title"><ion-icon name="logo-css3"></ion-icon></span>
-        </div>
-    )
-};
+// export default function Languages({ props }) {
+
+//     return (
+//         <>
+//         {props.map((language) => {
+//             return (
+//                 <p className="tag">{language}</p>
+//             )
+//         })}
+//         </>
+//     )
+
+// };
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,7 @@ export default function Projects() {
     var trafficJam = {
         title: "Traffic Jam",
         description: "This site utilized TicketMaster's API to show local events and save them within local storage.",
+        languages: ["HTML", "CSS"],
         repo: "https://github.com/eagersidekick/Traffic_Jam",
         site: "https://eagersidekick.github.io/Traffic_Jam/"
     }
@@ -12,6 +13,7 @@ export default function Projects() {
     var weatherDashboard = {
         title: "Weather Dashboard",
         description: "This project used a weather API to show a five day forecast for a given location.",
+        languages: "HTML",
         repo: "https://github.com/DaniSweets/Weather-Dashboard",
         site: "https://danisweets.github.io/Weather-Dashboard/"
     }
@@ -19,6 +21,7 @@ export default function Projects() {
     var campaignManager = {
         title: "Campaign Manager",
         description: "This application utilized React to create a virtual space for TTRPG groups.",
+        languages: "HTML",
         repo:"https://github.com/eagersidekick/Campaign_Manager",
         site:"https://campaign-manager.onrender.com/"
     }
@@ -26,6 +29,7 @@ export default function Projects() {
     var selfSite = {
         title: 'This Website',
         description: "This application uses React to show all my info with a sleek and minimalistic design.",
+        languages: "HTML",
         repo: "https://github.com/DaniSweets/SelfSite",
         site: "https://danisweetwood.com"
 
