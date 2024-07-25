70e161f8dbb04c4363de670f47e4f2b727de8ee3
Dani <danisweetwood@gmail.com>
2024-07-25 13:11:56 -0500

    updated aboutme wording

--- src/components/AboutMeMobile.jsx
+++ src/components/AboutMeMobile.jsx
@@ -1,7 +1,7 @@
 export default function AboutMeMobile() {
     return (
         <div className="mb-4">
-            <p>I am a Front End Software Engineer with a background in healthcare. I recently graduated the University of Kansas' full-stack web development bootcamp, where I created responsive web applications utilizing React.js, JavaScript, Node.js, as well as many CSS frameworks. Due to my experience in the healthcare field, I excel at time management, efficiency, and accuracy in all my work.</p>
+            <p>I am a Front End Software Engineer with a background in healthcare. I recently completed the University of Kansas' full-stack web development certification course, where I created responsive web applications utilizing React.js, JavaScript, Node.js, as well as many CSS frameworks. Due to my experience in the healthcare field, I excel at time management, efficiency, and accuracy in all my work.</p>
         </div>
     )
 }
